cd4fd1d035f8fcaf14676ac2c3361746917a3c88
René Sprotte <renspr@gmail.com>
2018-11-14 14:55:58 +0100

    Fix index for ES 6.x

--- etc/mapping_es6.rb
+++ etc/mapping_es6.rb
@@ -1,70 +1,316 @@
 MAPPING_ES6 = {
-  aleph_record: {
-    properties: {
-      title: {
-        type: "text",
-        copy_to: "suggest"
+  "aleph_record": {
+    "properties": {
+      "abstract": {
+        "type": "text"
       },
-      creator_contributor_display: {
-        type: "text",
-        copy_to: "suggest"
+      "additional_data": {
+        "properties": {
+          "local_comment": {
+            "type": "text"
+          }
+        }
       },
-      subject: {
-        type: "text",
-        index: false,
-        copy_to: "suggest"
+      "cataloging_date": {
+        "type": "date",
+        "format": "strict_date_optional_time||epoch_millis"
       },
-      suggest: {
-        type: "text",
-        analyzer: "minimal" # It is the field value which is copied, not the terms (which result from the analysis process).
+      "creation_date": {
+        "type": "keyword"
+      },
+      "creationdate": {
+        "type": "keyword"
+      },
+      "creationdate_facet": {
+        "type": "long"
+      },
+      "creationdate_search": {
+        "type": "keyword"
+      },
+      "creator_contributor_display": {
+        "type": "text"
+      },
+      "creator_contributor_facet": {
+        "type": "keyword"
+      },
+      "creator_contributor_search": {
+        "type": "text"
+      },
+      "ddc": {
+        "type": "keyword"
+      },
+      "delivery_category_facet": {
+        "type": "keyword"
+      },
+      "description": {
+        "type": "text"
+      },
+      "edition": {
+        "type": "text"
+      },
+      "erscheinungsform_facet": {
+        "type": "keyword"
+      },
+      "format": {
+        "type": "text",
+        "index": false
+      },
+      "fulltext_links": {
+        "type": "text"
+      },
+      "ht_number": {
+        "type": "keyword"
+      },
+      "id": {
+        "type": "keyword"
+      },
+      "ils_record_id": {
+        "type": "keyword"
+      },
+      "inhaltstyp_facet": {
+        "type": "keyword"
+      },
+      "is_part_of": {
+        "properties": {
+          "ht_number": {
+            "type": "keyword"
+          },
+          "label": {
+            "type": "text"
+          },
+          "label_additions": {
+            "type": "text"
+          },
+          "volume_count": {
+            "type": "text"
+          }
+        }
+      },
+      "is_secondary_form": {
+        "type": "boolean"
+      },
+      "is_suborder": {
+        "type": "boolean"
+      },
+      "is_superorder": {
+        "type": "boolean"
+      },
+      "isbn": {
+        "type": "text",
+        "index": false
+      },
+      "isbn_search": {
+        "type": "text"
+      },
+      "issn": {
+        "type": "text"
+      },
+      "journal_stock": {
+        "properties": {
+          "comment": {
+            "type": "text",
+            "index": false
+          },
+          "gaps": {
+            "type": "text",
+            "index": false
+          },
+          "leading_text": {
+            "type": "text",
+            "index": false
+          },
+          "signature": {
+            "type": "text",
+            "index": false
+          },
+          "stock": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "language": {
+        "type": "keyword"
+      },
+      "language_facet": {
+        "type": "keyword"
+      },
+      "ldsX": {
+        "type": "text"
+      },
+      "link_to_toc": {
+        "type": "text",
+        "index": false
+      },
+      "local_comment": {
+        "type": "text"
+      },
+      "materialtyp_facet": {
+        "type": "keyword"
+      },
+      "notation": {
+        "type": "text",
+        "analyzer": "minimal"
+      },
+      "notation_facet": {
+        "type": "keyword"
+      },
+      "notation_sort": {
+        "type": "keyword"
+      },
+      "publisher": {
+        "type": "text"
+      },
+      "relation": {
+        "properties": {
+          "ht_number": {
+            "type": "keyword"
+          },
+          "label": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "resource_link": {
+        "type": "text",
+        "index": false
+      },
+      "resource_links": {
+        "properties": {
+          "label": {
+            "type": "text",
+            "index": false
+          },
+          "url": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "secondary_form_creationdate": {
+        "type": "keyword"
+      },
+      "secondary_form_isbn": {
+        "type": "text"
+      },
+      "secondary_form_physical_description": {
+        "type": "text"
+      },
+      "secondary_form_preliminary_phrase": {
+        "type": "text"
+      },
+      "secondary_form_publisher": {
+        "type": "text"
+      },
+      "secondary_form_relation": {
+        "properties": {
+          "ht_number": {
+            "type": "keyword"
+          },
+          "label": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "secondary_form_superorder": {
+        "properties": {
+          "ht_number": {
+            "type": "keyword"
+          },
+          "label": {
+            "type": "text",
+            "index": false
+          },
+          "volume_count": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "selection_code": {
+        "type": "text",
+        "analyzer": "minimal"
+      },
+      "short_title": {
+        "type": "text"
+      },
+      "short_title_sort": {
+        "type": "keyword"
+      },
+      "signature": {
+        "type": "text",
+        "index": false
+      },
+      "signature_search": {
+        "type": "text"
+      },
+      "source": {
+        "properties": {
+          "counting": {
+            "type": "text",
+            "index": false
+          },
+          "ht_number": {
+            "type": "keyword"
+          },
+          "label": {
+            "type": "text",
+            "index": false
+          },
+          "volume": {
+            "type": "text",
+            "index": false
+          }
+        }
+      },
+      "status": {
+        "type": "keyword"
+      },
+      "subject": {
+        "type": "text",
+        "index": false
+      },
+      "subject_facet": {
+        "type": "keyword"
+      },
+      "subject_search": {
+        "type": "text"
+      },
+      "superorder": {
+        "type": "keyword"
+      },
+      "title": {
+        "type": "text",
+        "index": false
+      },
+      "title_display": {
+        "type": "text",
+        "index": false
+      },
+      "title_search": {
+        "type": "text"
+      },
+      "title_sort": {
+        "type": "keyword"
+      },
+      "toc": {
+        "type": "text"
+      },
+      "volume_count": {
+        "type": "text",
+        "index": false
+      },
+      "volume_count_sort": {
+        "type": "keyword"
+      },
+      "volume_count_sort2": {
+        "type": "keyword"
+      },
+      "zdb_id": {
+        "type": "keyword"
       }
-    },
-    dynamic_templates: [
-      {
-        cataloging_date: {
-          match: "cataloging_date",
-          mapping: {
-            type: "date"
-          }
-        }
-      },
-      {
-        nested_fields: {
-          match: "additional_data|relation|secondary_form_superorder|is_part_of",
-          match_pattern: "regex",
-          mapping: {
-            type: "object"
-          }
-        }
-      },
-      {
-        minimal_analyzed_fields: {
-          match: "notation|selection_code",
-          match_pattern: "regex",
-          mapping: {
-            analyzer: "minimal"
-          }
-        }
-      },
-      {
-        non_analyzed_fields: {
-          match: ".+_facet|.+_sort|.+_sort2|ht_number|.+_id|id|ddc|status|superorder",
-          match_pattern: "regex",
-          mapping: {
-            index: false
-          }
-        }
-      },
-      {
-        # these fields are display only or have ..._search counterparts
-        non_indexed_field: {
-          match: "isbn|format|link_to_toc|resource_link|signature|subject|title",
-          match_pattern: "regex",
-          mapping: {
-            index: false
-          }
-        }
-      }
-    ]
+    }
   }
 }
